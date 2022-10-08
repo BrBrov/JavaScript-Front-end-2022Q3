eb1beb07cfb67eab9081b4df7f677da4727271e4
brbrov <epoc@list.ru>
2022-10-08 16:14:18 +0300

    feat: Create PopUp for 640px

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -323,6 +323,16 @@ class Testimonials {
         this.elems[2].remove();
         this.elems[1].after(saver);
     }
+    setTabletDesktop(){
+        let saver = this.elems[0].cloneNode(true);
+        this.elems[0].remove();
+        this.elems[this.elems.length - 1].after(saver);
+        saver = this.elems[1].cloneNode(true);
+        this.elems[1].remove();
+        this.elems[this.elems.length - 1].after(saver);
+        saver = this.elems[3].cloneNode(true);
+        this.elems[0].after(saver);
+    }
 
     addListener() {
         this.scrollLine.addEventListener('input', this._workerSlider.bind(this));
@@ -365,28 +375,115 @@ class Testimonials {
     }
 }
 
+class PopUp{
+    constructor() {
+        this.menu = document.querySelector('.description');
+        this._createPopUp();
+    }
+    _createPopUp(){
+        this.background = document.createElement('div');
+        this.background.className = 'popup-background';
+        this.block = document.createElement('div');
+        this.block.className = 'popup-block';
+        let elem = document.createElement('div');
+        elem.className = 'logo-container-popup';
+        this.block.appendChild(elem);
+        elem = document.createElement('div');
+        elem.className = 'logo-popup';
+        this.block.children[0].appendChild(elem);
+        elem = document.createElement('span');
+        elem.className = 'logo-title-popup';
+        elem.textContent = 'Pet Story online';
+        this.block.children[0].children[0].appendChild(elem);
+        elem = document.createElement('div');
+        elem.className = 'popup-close';
+        this.block.children[0].appendChild(elem);
+        elem = document.createElement('div');
+        elem.className = 'popup-container';
+        this.block.appendChild(elem);
+        let nav = document.createElement('ul');
+        nav.className = 'navigation-popup';
+        this.block.children[1].appendChild(nav);
+        elem = document.createElement('li');
+        elem.className = 'popup-list';
+        for(let i=0; i < 6; i++){
+            let saver = elem.cloneNode(true);
+            nav.appendChild(saver);
+        }
+        elem = document.createElement('a');
+        elem.className = 'popup-link';
+        let names = ['About', 'Map', 'Zoos', 'Donate', 'Contact us', 'Designed by ©'];
+        for (let i = 0; i < 6; i++) {
+            let saver = elem.cloneNode(true);
+            saver.textContent = names[i];
+            saver.href = '#';
+            nav.children[i].appendChild(saver);
+        }
+        nav.children[0].children[0].href = 'index.html';
+        nav.children[3].children[0].href = '../donate/index.html';
+        nav.children[5].children[0].href = 'https://www.figma.com/file/ypzT9idgAILaSRVRmDAJxn/online-zoo-3-weeks';
+    }
+    listener(){
+        this.menu.addEventListener('touchstart', this._processing.bind(this));
+    }
+    _processing(){
+            this.menu.addEventListener('touchend', this._showPopUp.bind(this));
+            this.background.addEventListener('touchstart',(ev)=>{
+                ev.stopPropagation();
+                console.log(ev.target.className);
+                switch(ev.target.className){
+                    case 'popup-close':
+                    case 'popup-background':
+                        this._closePopUp();
+                        break;
+                    case 'logo-popup':
+                    case 'logo-title-popup':
+                        window.location.href = 'index.html';
+                        break;
+                }
+            })
+    }
+    _showPopUp(ev){
+        let page = document.querySelector('.page');
+        page.children[0].before(this.background);
+        this.background.appendChild(this.block);
+        let open = new KeyframeEffect(this.block,[{top: '-12%'},{top: '0'}],{duration: 1000, fill: 'forwards'});
+        let anim = new Animation(open);
+        anim.play();
+    }
+    _closePopUp(ev){
+        let close = new KeyframeEffect(this.block, [{top: '0'}, {top:"-12%"}], {duration: 1000, fill: 'forwards'});
+        let anim = new Animation(close);
+        anim.play();
+        anim.onfinish = () =>{
+            this.block.remove();
+            this.background.remove();
+        }
+    }
+}
+
 window.addEventListener('load', () => {
     let active = new ActiveElem();
 })
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Content loaded');
+    let carousel = new Carousel();
+    let testimonials = new Testimonials();
     let width = Math.max(window.innerWidth, document.body.clientWidth || 0);
     if (width > 1001) {
-        let carousel = new Carousel();
-        let testimonials = new Testimonials();
         testimonials.addListener();
         carousel.setUpBig();
         carousel.addListenersDesktop();
     } else if (width < 1001 && width > 640) {
-        let carousel = new Carousel();
-        let testimonials = new Testimonials();
         testimonials.setSmallDesktop();
         testimonials.addListener();
         carousel.adaptiveDesktop();
         carousel.setUpBig();
         carousel.addListenersDesktop();
     } else if (width < 641 && width > 320) {
-        let carousel = new Carousel();
+        let popup = new PopUp();
+        popup.listener();
+        testimonials.setTabletDesktop();
         carousel.adaptiveDesktop();
         carousel.setUpSmall();
         carousel.addListenerTablet();
