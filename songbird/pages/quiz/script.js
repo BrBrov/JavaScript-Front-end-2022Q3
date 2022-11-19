8c5f70defa3f3f3489666dfca2bac2f94f429dfe
BrBrov <epoc@list.ru>
2022-11-19 12:36:14 +0300

    feat: feat start game

--- songbird/pages/quiz/script.js
+++ songbird/pages/quiz/script.js
@@ -8,17 +8,17 @@ let answerValue = null;
 
 let answer = null;
 
-async function locale(language){
-    if(language){
+async function locale(language) {
+    if (language) {
         localStorage.setItem('language', language);
-    }else{
+    } else {
         let locale = localStorage.getItem('language');
-        if(locale){
+        if (locale) {
             return locale
-        }else{
+        } else {
             if (/^ru\b/.test(navigator.language)) {
                 locale = 'ru';
-            }else{
+            } else {
                 locale = 'en'
             }
             localStorage.setItem('language', locale);
@@ -27,21 +27,21 @@ async function locale(language){
     }
 }
 
-async function getTestArray(){
+async function getTestArray() {
     let test = []
-    birdsDataEn.forEach((bird, i) =>{
-        let ctrl = new Set([0,1,2,3,4,5]);
+    birdsDataEn.forEach((bird, i) => {
+        let ctrl = new Set([0, 1, 2, 3, 4, 5]);
         let result = [];
         let tResult = [];
-        while (ctrl.size !== 0){
-            let index = Math.floor(Math.random()*6);
-            if(ctrl.has(index)){
+        while (ctrl.size !== 0) {
+            let index = Math.floor(Math.random() * 6);
+            if (ctrl.has(index)) {
                 result.push(bird[index]);
                 tResult.push(birdsData[i][index])
                 ctrl.delete(index);
             }
         }
-        let number = Math.ceil(Math.random()*6);
+        let number = Math.ceil(Math.random() * 6);
         let obj = {
             en: result,
             ru: tResult,
@@ -49,71 +49,76 @@ async function getTestArray(){
         };
         test.push(obj);
     })
-    
+
 
     return test;
 }
 
-async function translator(lanquage, testData){
+async function translator(lanquage, testData) {
     let ru = ["Главная", "Тест", "Галерея", "RU", "Счёт", "Разминка", "Воробьиные", "Лесные птицы", "Певчие птицы", "Хищные птицы", "Морские птицы", "Следующий", "Все права защищены ©, 2022"];
     let en = ["Main", "Quiz", "Gallery", "EN", "Score", "Warm-up", "Sparrows", "Forest birds", "Songbirds", "Birds of prey", "Seabirds", "Next question", "All right reserved ©, 2022"];
     let local = (lanquage === 'en') ? en : ru;
-    console.log(testData);
     let test = testData[answerCount];
-    test = test[lanquage]
-    console.log(test);
+    test = test[lanquage];
     let elemsArray = [];
 
     let elem = document.querySelectorAll(".nav-link");
-    elem.forEach(el=>{
+    elem.forEach(el => {
         elemsArray.push(el);
     })
     elemsArray.push(document.querySelector(".language"));
     elemsArray.push(document.querySelector(".score-text"));
     elem = document.querySelectorAll(".test-label");
-    elem.forEach(el=>{
+    elem.forEach(el => {
         elemsArray.push(el);
     })
     elemsArray.push(document.querySelector(".next-test"));
     elemsArray.push(document.querySelector(".git-text"));
-    elemsArray.forEach((elment, i)=>{
+    elemsArray.forEach((elment, i) => {
         elment.textContent = local[i];
     })
 
     elemsArray = [];
 
     elem = document.querySelectorAll(".answer");
-    elem.forEach((el, i)=>{
+    elem.forEach((el, i) => {
         el.textContent = test[i].name;
     })
     answer = testData[answerCount].answer;
-    if(startGame){
+    if (startGame) {
         document.querySelector(".question-answer").textContent = test[answer].name;
     }
-    if(startGame === null && answerValue !== null){
+    if (startGame === null && answerValue !== null) {
         document.querySelector(".answer-name").textContent = test[answerValue].name;
         document.querySelector(".latin-name").textContent = test[answerValue].species;
         document.querySelector(".answer-description").textContent = test[answerValue].description;
     }
-    if(lanquage === 'en'){
+    if (lanquage === 'en') {
         document.querySelector(".text-welcom").textContent = "Choose an answer option to start the quiz";
-    }else{
+    } else {
         document.querySelector(".text-welcom").textContent = "Выберите вариант ответа для начала викторины";
     }
-
-    console.log(testData);
 }
-async function goToMain(ev){
+async function goToMain(ev) {
     ev.stopPropagation();
     localStorage.setItem('score', '0');
     localStorage.setItem('mode', 'null');
     location.href = "../main/index.html";
 }
-async function setQuestion(){
-    
+async function quizProcessing(ev) {
+    ev.stopPropagation();
+    console.log(ev.target.parentElement);
+    if (ev.target.parentElement.dataset.item || ev.target.dataset.item) {
+        if (startGame !== null) {
+            document.querySelector(".welcom-game").style.display = 'none';
+            document.querySelector(".answer-comment").style.display = 'flex';
+            startGame = null;
+        }
+    }
+
 }
 
-window.addEventListener('DOMContentLoaded', async () =>{
+window.addEventListener('DOMContentLoaded', async () => {
     localStorage.setItem('score', '0');
     localStorage.setItem('mode', null);
     let lang = await locale();
@@ -121,48 +126,50 @@ window.addEventListener('DOMContentLoaded', async () =>{
     translator(lang, testData);
 })
 
-window.addEventListener('load', async()=>{
+window.addEventListener('load', async () => {
     let nav = document.querySelectorAll(".nav-link");
     nav[0].addEventListener('click', goToMain)
-    nav[1].addEventListener('click', (ev)=>{
+    nav[1].addEventListener('click', (ev) => {
         ev.stopPropagation();
         location.reload();
     });
-    nav[2].addEventListener('click', (ev)=>{
+    nav[2].addEventListener('click', (ev) => {
         ev.stopPropagation();
         localStorage.setItem('mode', 'gallery');
         location.href = "../result/index.html";
     })
     let git = document.querySelector(".git-logo");
-    git.addEventListener('click', ()=>{
+    git.addEventListener('click', () => {
         location.href = "https://github.com/BrBrov";
     })
     let rs = document.querySelector(".rsschool");
-    rs.addEventListener('click', ()=>{
+    rs.addEventListener('click', () => {
         location.href = "https://rs.school/js/";
     })
     let langBtn = document.querySelector(".language");
-    langBtn.addEventListener('click', async ()=>{
+    langBtn.addEventListener('click', async () => {
         let lang = await locale();
-        if(lang === 'ru'){
+        if (lang === 'ru') {
             lang = 'en';
-        }else{
+        } else {
             lang = 'ru';
         }
         locale(lang);
-        let startRotate  = new KeyframeEffect(langBtn,
-            [{transform:'rotateY(0)'},{transform: 'rotateY(90deg)'}],
-            {duration: 500, fill: "forwards"});
+        let startRotate = new KeyframeEffect(langBtn,
+            [{ transform: 'rotateY(0)' }, { transform: 'rotateY(90deg)' }],
+            { duration: 500, fill: "forwards" });
         let endRotate = new KeyframeEffect(langBtn,
-            [{transform:'rotateY(90deg)'},{transform: "rotateY(0deg)"}],
-            {duration: 500, fill: "forwards"});
+            [{ transform: 'rotateY(90deg)' }, { transform: "rotateY(0deg)" }],
+            { duration: 500, fill: "forwards" });
         let anim = new Animation(startRotate, document.timeline);
         anim.play();
-        anim.onfinish = async () =>{
+        anim.onfinish = async () => {
             await translator(lang, testData);
             anim = new Animation(endRotate, document.timeline);
             anim.play();
         }
     })
+    let answersBoard = document.querySelector(".answers");
+    answersBoard.addEventListener('click', quizProcessing);
 })
 
