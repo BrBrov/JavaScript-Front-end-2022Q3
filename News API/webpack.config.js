a2e31cdcc0f2dbad5fa2bc563bb8aa526f657f5c
brbrov <epoc@list.ru>
2022-12-10 17:55:51 +0300

    docs: add rules from JS to TS

--- News API/webpack.config.js
+++ News API/webpack.config.js
@@ -2,12 +2,17 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader'
+            },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -22,7 +27,7 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts','.js'],
     },
     output: {
         filename: 'index.js',
@@ -34,6 +39,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslintPlugin({ extensions: 'ts' }),
     ],
 };
 
