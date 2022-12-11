8d4c4c8d98c981d5734b940f1265ccb138bc6b61
brbrov <epoc@list.ru>
2022-12-11 23:28:28 +0300

    docs: add link to proxy

--- News API/src/components/controller/appLoader.ts
+++ News API/src/components/controller/appLoader.ts
@@ -2,7 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
+            //https://newsapi.org/v2/
             apiKey: '500223348eb84205b4ecefe586bd0fff', // получите свой ключ https://newsapi.org/
         });
     }
